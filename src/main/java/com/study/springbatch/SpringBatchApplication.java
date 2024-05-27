bae4231aada3e85f220173f51e142b5fc81c3c75
nadoran78 <nadoran78@gmail.com>
2024-05-27 23:35:58 +0900

    two minute tutorial

--- src/main/java/com/study/springbatch/SpringBatchApplication.java
+++ src/main/java/com/study/springbatch/SpringBatchApplication.java
@@ -1,13 +1,22 @@
 package com.study.springbatch;
 
-import org.springframework.boot.SpringApplication;
+import com.study.springbatch.two_minute_tutorial.HelloWorldJobConfiguration;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringBatchApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpringBatchApplication.class, args);
+  public static void main(String[] args) throws Exception {
+    ApplicationContext context = new AnnotationConfigApplicationContext(
+        HelloWorldJobConfiguration.class);
+    JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+    Job job = context.getBean(Job.class);
+    jobLauncher.run(job, new JobParameters());
   }
 
 }
