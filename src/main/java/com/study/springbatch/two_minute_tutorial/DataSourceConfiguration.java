ecea0f8003765f1ff23f4385c59ecdaeeb10d7f2
nadoran78 <nadoran78@gmail.com>
2024-05-27 23:35:53 +0900

    two minute tutorial

---
+++ src/main/java/com/study/springbatch/two_minute_tutorial/DataSourceConfiguration.java
@@ -0,0 +1,24 @@
+package com.study.springbatch.two_minute_tutorial;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+
+@Configuration
+public class DataSourceConfiguration {
+
+  @Bean
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder()
+        .addScript("/org/springframework/batch/core/schema-hsqldb.sql")
+        .build();
+  }
+
+  @Bean
+  public JdbcTransactionManager transactionManager(DataSource dataSource) {
+    return new JdbcTransactionManager(dataSource);
+  }
+
+}
